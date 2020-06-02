4e0eea2768fbadc9067282034858dc5610ce5ee6
Gaoxiang <605711567@qq.com>
2020-06-02 22:09:59 +0800

    feat-BFS | 最小开锁次数

---
+++ src/main/java/bfs/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,72 @@
+//给定一个二叉树，找出其最小深度。 
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例: 
+//
+// 给定二叉树 [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最小深度 2. 
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+package bfs;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MinimumDepthOfBinaryTree {
+      public static void main(String[] args) {
+           Solution solution = new MinimumDepthOfBinaryTree().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        // 套用bfs模板
+        if (root == null) {
+            return 0;
+        }
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        // root本身算一层
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (cur.left == null && cur.right == null) {
+                    return depth;
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
