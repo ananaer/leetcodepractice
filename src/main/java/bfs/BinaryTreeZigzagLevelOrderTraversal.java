9686686f8c783220ee73b9a903e7bf20a64338a8
Gaoxiang <605711567@qq.com>
2020-06-07 22:24:16 +0800

    feat-BFS | 锯齿形遍历 双向队列

---
+++ src/main/java/bfs/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,86 @@
+//给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。 
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回锯齿形层次遍历如下： 
+//
+// [
+//  [3],
+//  [20,9],
+//  [15,7]
+//]
+// 
+// Related Topics 栈 树 广度优先搜索
+
+package bfs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+      public static void main(String[] args) {
+           Solution solution = new BinaryTreeZigzagLevelOrderTraversal().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        // 关键点 使用双端队列
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.offer(root);
+        boolean isOrder = true;
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            List<Integer> curList = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = isOrder ? deque.removeFirst() : deque.removeLast();
+                curList.add(cur.val);
+                if (isOrder) {
+                    if (cur.left != null) {
+                        deque.addLast(cur.left);
+                    }
+                    if (cur.right != null) {
+                        deque.addLast(cur.right);
+                    }
+                }else {
+                    if (cur.right != null) {
+                        deque.addFirst(cur.right);
+                    }
+                    if (cur.left != null) {
+                        deque.addFirst(cur.left);
+                    }
+
+                }
+
+            }
+            isOrder = !isOrder;
+            result.add(curList);
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
