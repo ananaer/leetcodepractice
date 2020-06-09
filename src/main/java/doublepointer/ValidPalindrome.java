4bcb02742bcfeb88846b18d8324cb0c6a1780fe7
Gaoxiang <605711567@qq.com>
2020-06-09 23:06:06 +0800

    feat-双指针 | 判断回文

---
+++ src/main/java/doublepointer/ValidPalindrome.java
@@ -0,0 +1,51 @@
+//给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。 
+//
+// 说明：本题中，我们将空字符串定义为有效的回文串。 
+//
+// 示例 1: 
+//
+// 输入: "A man, a plan, a canal: Panama"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: "race a car"
+//输出: false
+// 
+// Related Topics 双指针 字符串
+
+package doublepointer;
+  public class ValidPalindrome {
+      public static void main(String[] args) {
+           Solution solution = new ValidPalindrome().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.length() == 0) {
+            return true;
+        }
+        int left = 0;
+        int right = s.length() - 1;
+        while (left <= right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            } else {
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
