626320d29aa3b8ec1390ab5f0b1ce895735dfe0e
Gaoxiang <605711567@qq.com>
2020-07-06 21:56:02 +0800

    feat-单调队列 | 滑动窗口中的最大值

---
+++ src/main/java/subsequence/HuaDongChuangKouDeZuiDaZhiLcof.java
@@ -0,0 +1,68 @@
+//给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。 
+//
+// 示例: 
+//
+// 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+//输出: [3,3,5,5,6,7] 
+//解释: 
+//
+//  滑动窗口的位置                最大值
+//---------------               -----
+//[1  3  -1] -3  5  3  6  7       3
+// 1 [3  -1  -3] 5  3  6  7       3
+// 1  3 [-1  -3  5] 3  6  7       5
+// 1  3  -1 [-3  5  3] 6  7       5
+// 1  3  -1  -3 [5  3  6] 7       6
+// 1  3  -1  -3  5 [3  6  7]      7 
+//
+// 
+//
+// 提示： 
+//
+// 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。 
+//
+// 注意：本题与主站 239 题相同：https://leetcode-cn.com/problems/sliding-window-maximum/ 
+// Related Topics 队列 Sliding Window
+
+package subsequence;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class HuaDongChuangKouDeZuiDaZhiLcof {
+      public static void main(String[] args) {
+           Solution solution = new HuaDongChuangKouDeZuiDaZhiLcof().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // 要点1.使用单向队列 大->小
+        //     2.单向队列中存的是下标，这样才能判断滑动窗口是不是略过了
+        if (nums == null || nums.length == 0) {
+            return new int[]{};
+        }
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            // 当前要插入的值比队尾元素大等时 移除队尾元素
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+            // 注意存的是下标
+            deque.addLast(i);
+            // 判断窗口大小 移除窗口外的元素
+            if (deque.peekFirst() == i - k) {
+                deque.pollFirst();
+            }
+            // 如果窗口形成则加入结果
+            if (i + 1 >= k) {
+                res[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
