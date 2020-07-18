3c54f271948b0393189b72497179e4479f288409
Gaoxiang <605711567@qq.com>
2020-07-18 23:10:47 +0800

    feat-树 || 求树高

---
+++ src/main/java/tree/BalancedBinaryTree.java
@@ -0,0 +1,73 @@
+//给定一个二叉树，判断它是否是高度平衡的二叉树。 
+//
+// 本题中，一棵高度平衡二叉树定义为： 
+//
+// 
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。 
+// 
+//
+// 示例 1: 
+//
+// 给定二叉树 [3,9,20,null,null,15,7] 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回 true 。 
+// 
+//示例 2: 
+//
+// 给定二叉树 [1,2,2,3,3,null,null,4,4] 
+//
+//        1
+//      / \
+//     2   2
+//    / \
+//   3   3
+//  / \
+// 4   4
+// 
+//
+// 返回 false 。 
+//
+// 
+// Related Topics 树 深度优先搜索
+
+package tree;
+  public class BalancedBinaryTree {
+      public static void main(String[] args) {
+           Solution solution = new BalancedBinaryTree().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return Math.abs(dfs(root.left)-dfs(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    // 求树高
+
+    public int dfs(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return Math.max(dfs(root.right) , dfs(root.left)) + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
