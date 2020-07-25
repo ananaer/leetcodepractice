861b77ae620bee5d822037c298341d0f491d3d99
Gaoxiang <605711567@qq.com>
2020-07-25 20:04:23 +0800

    feat- 双指针 | 链表相交处

--- src/main/java/jianzhioffer/CongWeiDaoTouDaYinLianBiaoLcof.java
+++ src/main/java/jianzhioffer/CongWeiDaoTouDaYinLianBiaoLcof.java
@@ -33,8 +33,8 @@ public class CongWeiDaoTouDaYinLianBiaoLcof {
  * }
  */
  public class ListNode {
-      int val;
-      ListNode next;
+      public int val;
+      public ListNode next;
       ListNode(int x) { val = x; }
   }
 class Solution {
