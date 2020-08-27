3d2a055f5cf004419ed2c6e0a186bf31e4991622
Gaoxiang <605711567@qq.com>
2020-08-27 21:30:08 +0800

    feat-根据前序与中序遍历确定二叉树

--- src/main/java/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -40,28 +40,30 @@ class Solution {
         // 整体思路 先从先序遍历中找到根节点
         // 在从中序遍历中找到根节点确定根节点的左子树与右子树
         // 对左子树右子树递归此过程
-        HashMap<Integer, Integer> inOrder = new HashMap<>();
-        for (int i = 0; i < inorder.length; i++) {
-            inOrder.put(inorder[i], i);
-        }
-        return buildTree(preorder, 0, preorder.length , inOrder, 0, inorder.length);
+        return buildTreeHelper(preorder, 0, preorder.length, inorder, 0, inorder.length);
     }
 
-    private TreeNode buildTree(int[] preorder, int preStart, int preEnd, HashMap<Integer, Integer> inOrder, int inStart, int inEnd) {
-        if (preStart == preEnd) {
+    private TreeNode buildTreeHelper(int[] preorder, int p_start, int p_end, int[] inorder, int i_start, int i_end) {
+        // preorder 为空，直接返回 null
+        if (p_start == p_end) {
             return null;
         }
-        int root = preorder[preStart];
-        // 找到根节点在中序遍历中的位置
-        int rootIndex = inOrder.get(root);
-        TreeNode rootTree = new TreeNode(root);
-        int leftLength = rootIndex - inStart + 1;
-        TreeNode left = buildTree(preorder, preStart + 1, preStart + leftLength, inOrder, inStart, rootIndex);
-        TreeNode right = buildTree(preorder, preStart + leftLength, preEnd, inOrder, rootIndex + 1, inEnd);
-        rootTree.left = left;
-        rootTree.right = right;
-        return rootTree;
-
+        int root_val = preorder[p_start];
+        TreeNode root = new TreeNode(root_val);
+        //在中序遍历中找到根节点的位置
+        int i_root_index = 0;
+        for (int i = i_start; i < i_end; i++) {
+            if (root_val == inorder[i]) {
+                i_root_index = i;
+                break;
+            }
+        }
+        int leftNum = i_root_index - i_start;
+        //递归的构造左子树
+        root.left = buildTreeHelper(preorder, p_start + 1, p_start + leftNum + 1, inorder, i_start, i_root_index);
+        //递归的构造右子树
+        root.right = buildTreeHelper(preorder, p_start + leftNum + 1, p_end, inorder, i_root_index + 1, i_end);
+        return root;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
